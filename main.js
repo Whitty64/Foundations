f9cb1491ea2d15eeb86a5ff812bd0f8173b1ba36
Taylor <taywhitfield1995@gmail.com>
2019-04-06 13:42:34 -0400

    Nearly There

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Logging from the console');
+var firstNumber= 50;
+var secondNumber= 100;
+var thirdNumber= 500;
+var sum = secondNumber + firstNumber + thirdNumber;
+console.log(sum > 400)
