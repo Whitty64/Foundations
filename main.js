6b3468243b4da8eff0f5ba0bb1b6e6b04dbdbc63
Taylor <taywhitfield1995@gmail.com>
2019-04-10 13:32:04 -0400

    sorry Justin

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-console.log('Logging from the console');
+/*console.log('Logging from the console');
 var firstNumber= 50;
 var secondNumber= 100;
 var thirdNumber= 500;
 var sum = secondNumber + firstNumber + thirdNumber;
-console.log(sum > 400)
+console.log(sum > 400)*/
+/*.text to change button text*/
+
+function handleClick(event){
+  event.preventDefault();
+  $(".buyit").text("ADDED TO CART!");
+}
+
+
+/*function changeBack() {
+ setTimeout($(".buyit".text), 2000);
+}
+*/
